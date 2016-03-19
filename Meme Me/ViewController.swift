50aeb2d6d90206ea9897c40306d0d17e1359bd99
Mark Lindamood <mark.lindamood@yahoo.com>
2016-03-19 16:29:02 -0700

    Adding 'Main.storyboard' and 'ViewController'

--- Meme Me/ViewController.swift
+++ Meme Me/ViewController.swift
@@ -8,18 +8,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var albumButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
-
-
+    
+    enum Sources {
+        case PhotoLibrary
+        case Camera
+    }
+    
+    
+    @IBAction func cancelImagePicker(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(sender: AnyObject) {
+        switchSource(Sources.Camera)
+        /*let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        presentViewController(imagePicker, animated: true, completion: nil)*/
+    }
+    
+    @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
+        switchSource(Sources.PhotoLibrary)
+        /*let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)*/
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+            imagePickerView.contentMode = .ScaleToFill
+            dismissViewControllerAnimated(true, completion: nil)
+            
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func switchSource(source: Sources) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        switch source {
+        case Sources.PhotoLibrary:
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        case Sources.Camera:
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            
+        }
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
 }
+
 
