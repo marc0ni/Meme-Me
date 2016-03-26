4409862a17f83090f01049addf672ca235e57d33
Mark Lindamood <mark.lindamood@yahoo.com>
2016-03-26 12:04:22 -0700

    Changes to keyboard,  nav bar and toolbar

--- Meme Me/ViewController.swift
+++ Meme Me/ViewController.swift
@@ -14,10 +14,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var albumButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var topToolBar: UINavigationItem!
+    @IBOutlet weak var bottomToolBar: UIToolbar!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     
-    // MARK: - Constants
+    ///// MARK: - Constants & Set-up
     //Copied from https://github.com/mrecachinas/MemeMeApp/blob/master/MemeMe/MemeEditorViewController.swift
     let TOP_DEFAULT_TEXT = "TOP"
     let BOTTOM_DEFAULT_TEXT = "BOTTOM"
@@ -42,9 +45,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewWillAppear(animated: Bool) {
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        self.subscribeToKeyboardNotifications()
     }
     
-    ////// TextField Functions
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    ////// MARK: TextField Functions
     let memeTextDelegate = MemeTextFieldDelegate()
     
     //Modified from https://github.com/mrecachinas/MemeMeApp/blob/master/MemeMe/MemeEditorViewController.swift
@@ -57,18 +66,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         textField.delegate = self
     }
     
-    
-    ///// ImagePicker Functions
-    /*enum Sources {
-        case PhotoLibrary
-        case Camera
-    }*/
-    
     func resetText() {
         topTextField.text = TOP_DEFAULT_TEXT
         bottomTextField.text = BOTTOM_DEFAULT_TEXT
     }
     
+    
+    ///// MARK: ImagePicker Functions
     @IBAction func cancelImagePicker(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
@@ -96,13 +100,107 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             imagePickerView.image = image
             imagePickerView.contentMode = .ScaleToFill
             dismissViewControllerAnimated(true, completion: nil)
-            
         }
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return [.AllButUpsideDown]
+    }
+
+    
+    ///// MARK: Keyboard Functions
+    // Copied from https://github.com/mrecachinas/MemeMeApp/blob/master/MemeMe/MemeEditorViewController.swift
+    func keyboardWillShow(notification: NSNotification){
+        if bottomTextField.isFirstResponder() {
+            if view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= getKeyboardHeight(notification)
+            }
+        }
+    }
+    
+    // Copied from https://github.com/mrecachinas/MemeMeApp/blob/master/MemeMe/MemeEditorViewController.swift
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    // Modified from https://github.com/mrecachinas/MemeMeApp/blob/master/MemeMe/MemeEditorViewController.swift
+    func getKeyboardHeight(notification:NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as!NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    // Copied from https://github.com/mrecachinas/MemeMeApp/blob/master/MemeMe/MemeEditorViewController.swift
+    func keyboardWillHide(notification: NSNotification){
+        self.view.frame.origin.y = 0
+    }
+    
+    // Copied from https://github.com/mrecachinas/MemeMeApp/blob/master/MemeMe/MemeEditorViewController.swift
+    func unsubscribeFromKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:UIKeyboardDidShowNotification, object:nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:UIKeyboardDidHideNotification, object:nil)
+
+    }
+    
+    
+    ///// MARK: Helpers for creating Meme
+    func hideStatusBarAndToolBar() {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        bottomToolBar.hidden = true
+    }
+    
+    func showStatusBarAndToolbar() {
+        self.navigationController?.navigationBarHidden = false
+        bottomToolBar.hidden = false
+    }
+    
+    func takeScreenshot() -> UIImage {
+        // uses the context approach
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.drawViewHierarchyInRect(view.frame, afterScreenUpdates: true)
+        let memedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return memedImage
+    }
+    
+    
+    ///// MARK: Memes Object Functions
+    struct Meme {
+        var topTextField: String
+        var bottomTextField:String
+        var pickerViewImage: UIImage
+        var memedImage: UIImage
+    }
+    
+    func save () {
+        // Create the meme
+        let memedImage = generateMemedImage()
+        _ = Meme(topTextField: topTextField.text!, bottomTextField: bottomTextField.text!, pickerViewImage:imagePickerView.image!, memedImage:memedImage)
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        hideStatusBarAndToolBar()
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawViewHierarchyInRect(self.view.frame,
+            afterScreenUpdates: true)
+        let memedImage : UIImage =
+        UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        showStatusBarAndToolbar()
+        
+        return memedImage
+    }
+    
 }
 
 
