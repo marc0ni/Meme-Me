b9fd2dfa1cd094f41d803c03dddc0345f07c2387
Mark Lindamood <mark.lindamood@yahoo.com>
2016-05-26 20:26:43 -0700

    Added "panAndScanButton" from cropping and scrolling image

--- Meme Me/MemeEditorViewController.swift
+++ Meme Me/MemeEditorViewController.swift
@@ -18,6 +18,8 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     @IBOutlet weak var albumButton: UIBarButtonItem!
     @IBOutlet weak var shareButton: UIBarButtonItem!
     @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var panAndScanButton: UIBarButtonItem!
+    
     
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var bottomToolBar: UIToolbar!
